47e3d13b2c564dd70904bfd30e6d340877bac43f
Damon Swayn <dswayn@email.com>
2013-07-29 19:33:31 +1000

    Initial Commit

---
+++ NQL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NQL
+//
+//  Created by Damon Swayn on 29/07/13.
+//  Copyright (c) 2013 Damon Swayn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
